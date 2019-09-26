12c3fe57c5d2446b550d441add0560d920e26ecd
Cangyu Lin <jefflin.je598@gmail.com>
2019-09-26 19:22:31 +0800

    enable content scripts

--- insert.js
+++ insert.js
@@ -1,16 +1,10 @@
-function DO(){
+
 console.log("insert javascript executed");
 var btn = document.createElement("BUTTON");   // Create a <button> element
 btn.innerHTML = "CLICK ME";                   // Insert text
 var post = document.getElementsByClassName("_5pcp _5lel _2jyu _232_");
 for (var i = 0; i<post.length;i++){
 post[i].append(btn);     // Append button to div
+console.log("button"+i+"created");
 }
-}
-var delayInMilliseconds = 1000; //1 second
-while(0==0){
-setTimeout(function() {
-  //your code to be executed after 1 second
-  DO();
-}, delayInMilliseconds);
-}
+
