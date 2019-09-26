17c7c0e2848f75ae45343f8a899ae0a0c3c26896
Cangyu Lin <jefflin.je598@gmail.com>
2019-09-26 11:55:31 +0800

    Add files via upload

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+console.log("background javascript executed!");
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript({
       file: "insert.js"
--- insert.js
+++ insert.js
@@ -1,3 +1,16 @@
+function DO(){
+console.log("insert javascript executed");
 var btn = document.createElement("BUTTON");   // Create a <button> element
 btn.innerHTML = "CLICK ME";                   // Insert text
-document.getElementsByClassName("_5pcp _5lel _2jyu _232_").appendChild(btn);     // Append button to div
+var post = document.getElementsByClassName("_5pcp _5lel _2jyu _232_");
+for (var i = 0; i<post.length;i++){
+post[i].append(btn);     // Append button to div
+}
+}
+var delayInMilliseconds = 1000; //1 second
+while(0==0){
+setTimeout(function() {
+  //your code to be executed after 1 second
+  DO();
+}, delayInMilliseconds);
+}
