a1e6ff7a37bb3c7c62f8e043c650b130dbe9e0e3
Cangyu Lin <jefflin.je598@gmail.com>
2019-09-26 10:56:35 +0800

    add button near the facebook post

--- popup.js
+++ popup.js
@@ -17,3 +17,9 @@ function parseURLParams(url) {
     }
     return parms;
 }
+
+function createButton(){
+    var btn = document.createElement("BUTTON");   // Create a <button> element
+    btn.innerHTML = "CLICK ME";                   // Insert text
+    document.getElementsByClassName("_5pcp _5lel _2jyu _232_").appendChild(btn);     // Append button to div
+}
