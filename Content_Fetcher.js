e8fa7bf6e303e76eb9c084c9ea1e5d4089318d9c
Cangyu Lin <jefflin.je598@gmail.com>
2019-09-30 14:33:16 +0800

    add a new function to fetch title from an URL

---
+++ Content_Fetcher.js
@@ -0,0 +1,20 @@
+function httpGet(theUrl)
+{
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", "http://textance.herokuapp.com/title" + theUrl, false );
+    xmlhttp.send();    
+}
