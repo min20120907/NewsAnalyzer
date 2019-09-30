65d89686957a2411848ee8fbf70e6cf2d38cd779
r96340 <r96340@gmail.com>
2019-09-30 16:12:44 +0800

    Update appendIFrame.js

--- appendIFrame.js
+++ appendIFrame.js
@@ -1,6 +1,7 @@
 console.log("insert javascript executed");
 var post = document.getElementsByClassName("_5pcp _5lel _2jyu _232_");
 var btn = [post.length];
+var ifrm = [post.length];
 for (var i = 0; i <= post.length - 1; i++) {
 	btn[i] = document.createElement("BUTTON")
     btn[i].innerHTML = "CLICK ME";                   // Insert text
