8af3398ea2754355b93b8d7713577e94be331cf7
Cangyu Lin <jefflin.je598@gmail.com>
2019-09-30 15:03:05 +0800

    update path

--- insert.js
+++ insert.js
@@ -1,10 +1,23 @@
-
 console.log("insert javascript executed");
-var btn = document.createElement("BUTTON");   // Create a <button> element
-btn.innerHTML = "CLICK ME";                   // Insert text
 var post = document.getElementsByClassName("_5pcp _5lel _2jyu _232_");
-for (var i = 0; i<post.length;i++){
-post[i].append(btn);     // Append button to div
-console.log("button"+i+"created");
+var btn = [post.length];
+for (var i = 0; i <= post.length - 1; i++) {
+	btn[i] = document.createElement("BUTTON")
+	btn[i].innerHTML = "CLICK ME";                   // Insert text
 }
 
+for (var i = 0; i <= post.length - 1; i++){
+	post[i].append(btn[i]);     // Append button to div
+	console.log("button"+i+"created");
+}
+var num = post.length - 1;
+
+post.length.addEventListener("change", autoappend);
+
+function autoappend(){
+	for (; num <= post.length - 1; i++){
+		post[i].append(btn[num]);     // Append button to div
+		console.log("button"+num+"created");
+	}
+	num = post.length - 1;
+}
