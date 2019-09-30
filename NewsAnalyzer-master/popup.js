c53c7ee39a86c4be1c1286174bef98dd6527cd07
fcwe1113 <55336153+fcwe1113@users.noreply.github.com>
2019-09-30 13:56:21 +0800

    make the buttons work
    yay

---
+++ NewsAnalyzer-master/popup.js
@@ -0,0 +1,25 @@
+function parseURLParams(url) {
+    var queryStart = url.indexOf("?") + 1,
+        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+        query = url.slice(queryStart, queryEnd - 1),
+        pairs = query.replace(/\+/g, " ").split("&"),
+        parms = {}, i, n, v, nv;
+
+    if (query === url || query === "") return;
+
+    for (i = 0; i < pairs.length; i++) {
+        nv = pairs[i].split("=", 2);
+        n = decodeURIComponent(nv[0]);
+        v = decodeURIComponent(nv[1]);
+
+        if (!parms.hasOwnProperty(n)) parms[n] = [];
+        parms[n].push(nv.length === 2 ? v : null);
+    }
+    return parms;
+}
+
+function createButton(){
+    var btn = document.createElement("BUTTON");   // Create a <button> element
+    btn.innerHTML = "CLICK ME";                   // Insert text
+    document.getElementsByClassName("_5pcp _5lel _2jyu _232_").appendChild(btn);     // Append button to div
+}
