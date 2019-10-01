9020604364a2caec4ad81310cdefe8a8d821d75b
Cangyu Lin <jefflin.je598@gmail.com>
2019-10-01 22:09:11 +0800

    Remove duplicated Directory

--- NewsAnalyzer-master/README.md
+++
@@ -1,2 +0,0 @@
-# NewsAnalyzer
-Using chrome extension to analyze the accuracy of the news.
--- NewsAnalyzer-master/background.js
+++
@@ -1,6 +0,0 @@
-console.log("background javascript executed!");
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript({
-      file: "insert.js"
-    });
-  });
