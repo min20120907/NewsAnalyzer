e60908f24b1290cb2a774cba6c84052a4fb58865
min20120907 <jefflin.je598@gmail.com>
2019-10-05 21:28:21 +0800

    getTittle update

--- insert.js
+++ insert.js
@@ -119,13 +119,23 @@ function searchBing(bingQuery) {
 }
 
 
-function getTitle(inputURL){
-    var request = new XMLHttpRequest();
-    request.addEventListener("load", function(evt){
-        console.log(evt);
-    }, false);
-    request.open('GET', inputURL, true);
-    request.send();
-    var givenTitle =  document.getElementsByTagName("pre")[0];
-    return givenTitle[0].innerHTML;
+function getTitle(theUrl)
+{
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", theUrl, false );
+    xmlhttp.send();    
 }
