384a62866db402f79b410eb7cbef0dc9b9f696e4
Cangyu Lin <jefflin.je598@gmail.com>
2020-09-10 11:05:05 +0800

    fixed double click problem

--- insert.js
+++ insert.js
@@ -2,11 +2,40 @@
 console.log("insert javascript executed");
 //Detecting the page if it is facebook.
 window.addEventListener("load", areYouInFacebook);
+for (var i = 0; i < 5; i++) {
+  var result = document.createElement("div");
+  result.id = "results-" + i;
+}
 
 //Elements proclaiming.
-var innerPost = document.getElementsByClassName("_1dwg _1w_m _q7o");
-var linkPost = document.getElementsByClassName("_52c6");
-var post = document.getElementsByClassName("_5pcp _5lel _2jyu _232_");
+//get posts by using style classes
+posts = document.getElementsByClassName("rq0escxv l9j0dhe7 du4w35lb hybvsw6c ue3kfks5 pw54ja7n uo3d90p7 l82x9zwi ni8dbmo4 stjgntxs k4urcfbm sbcfpzgs");
+innerPost = [];
+//filter it with check whether has content
+a = 0;
+for (var r = 0; r < posts.length; r++) {
+  if (posts[r].innerHTML.includes("oi732d6d ik7dh3pa d2edcug0 hpfvmrgz qv66sw1b c1et5uql a8c37x1j hop8lmos enqfppq2 e9vueds3 j5wam9gi knj5qynh m9osqain hzawbc8m")) {
+    innerPost[a] = posts[r]; //if it is a post add it
+    a++;
+  }
+}
+
+link_posts = document.getElementsByClassName("oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 a8c37x1j p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 p8dawk7l");
+linkPost = [];
+a = 0;
+for (var i = 0; i < link_posts.length; i++) {
+  //if it is link, then add it
+  if (link_posts[i].tagName == "A") {
+    linkPost[a] = link_posts[i];
+    a++;
+  }
+}
+
+var post = [];
+for (var i = 0; i < innerPost.length; i++) {
+  post[i] = innerPost[i].getElementsByClassName("oi732d6d ik7dh3pa d2edcug0 hpfvmrgz qv66sw1b c1et5uql a8c37x1j hop8lmos enqfppq2 e9vueds3 j5wam9gi knj5qynh m9osqain hzawbc8m")[0];
+}
+
 var btn = [innerPost.length];
 var ifrm = [innerPost.length];
 var g = [innerPost.length];
@@ -14,82 +43,133 @@ var table = [innerPost.length];
 var tbody = [innerPost.length];
 //var lpost = document.getElementsByClassName("_6m3 _--6");
 //var operator_s = 0;
-var entrSites=[
- "youthwant.com" ,
- "openfind.com" ,
- "timliao.com" ,
- "taconet.com" ,
- "gigigaga.com" ,
- "so-net.net" ,
- "don-net.com" ,
- "hongkong.com" ,
- "asiadog.com" ,
- "gotoya.com" ,
- "t2t.com" ,
- "moninet.com" ,
- "myweb.hinet.net" ,
- "udo.idv" ,
- "wutun.idv" ,
- "centurys.com.tw" ,
- "funf.tw" ,
- "portal.tw" ,
- "pig.tw" ,
- "taiwanurl.com" ,
- "263.net" ,
- "qq.com" ,
- "sohu.com" ,
- "china.com" ,
- "tom.com" ,
-"netease.com" ,
-"backchina.com" ,
-"gjj.cc" ,
-"qoos.com" ,
-"qknet.net" ,
-"facebook.com",
-"kknews.cc",
-"www.buzzhand.com",
-"www1.daliulian.net",
-"www.teepr.com",
-"bomb01.com",
-"cocomy",
-"coco01.net",
- "cocotw.net",
-"cocomy.net",
- "daliulian.net",
- "e04.tv",
- "how01.com",
-"juksy.com",
-"orange01",
-"push01.com",
-"read01",
-"thegreatdaily",
-"tw.gigacircle",
-"tw.ptt01",
-"twgreatdaily",
-"whatfunny",
-"momdata.blogspot.com",
-"onefunnyjoke.com",
-"gmter.com",
-"gigacircle.com",
-"share001.net",
- "shareba.com",
- "metalballs.com",
- "ptt01",
- "fun-vdo.com",
-"sos.tw",
-"zuopy.com",
- "eznewlife",
-"ezvivi.com",
-"youthwant.com.tw",
- "viralane.com",
- "newstube01",
- "eazon.com",
- "mama.tw",
-"cocomy",
-"baoxiaovideo.tv",
-"apple01.net",
-"likea.ezvivi.com"
+var entrSites = [
+  "youthwant.com",
+  "openfind.com",
+  "timliao.com",
+  "taconet.com",
+  "gigigaga.com",
+  "so-net.net",
+  "don-net.com",
+  "hongkong.com",
+  "asiadog.com",
+  "gotoya.com",
+  "t2t.com",
+  "moninet.com",
+  "myweb.hinet.net",
+  "udo.idv",
+  "wutun.idv",
+  "centurys.com.tw",
+  "funf.tw",
+  "portal.tw",
+  "pig.tw",
+  "taiwanurl.com",
+  "263.net",
+  "qq.com",
+  "sohu.com",
+  "china.com",
+  "tom.com",
+  "netease.com",
+  "backchina.com",
+  "gjj.cc",
+  "qoos.com",
+  "qknet.net",
+  "facebook.com",
+  "kknews.cc",
+  "www.buzzhand.com",
+  "www1.daliulian.net",
+  "www.teepr.com",
+  "bomb01.com",
+  "cocomy",
+  "coco01.net",
+  "cocotw.net",
+  "cocomy.net",
+  "daliulian.net",
+  "e04.tv",
+  "how01.com",
+  "juksy.com",
+  "orange01",
+  "push01.com",
+  "read01",
+  "thegreatdaily",
+  "tw.gigacircle",
+  "tw.ptt01",
+  "twgreatdaily",
+  "whatfunny",
+  "momdata.blogspot.com",
+  "onefunnyjoke.com",
+  "gmter.com",
+  "gigacircle.com",
+  "share001.net",
+  "shareba.com",
+  "metalballs.com",
+  "ptt01",
+  "fun-vdo.com",
+  "sos.tw",
+  "zuopy.com",
+  "eznewlife",
+  "ezvivi.com",
+  "youthwant.com.tw",
+  "viralane.com",
+  "newstube01",
+  "eazon.com",
+  "mama.tw",
+  "cocomy",
+  "baoxiaovideo.tv",
+  "apple01.net",
+  "likea.ezvivi.com",
+  "facebook.com",
+  "kknews.cc",
+  "google.com",
+  "yahoo.com",
+  "hinet.net",
+  "msn.com",
+  "pchome.com",
+  "yam.com",
+  "sina.com",
+  "cnet.com",
+  "seed.net.",
+  "url.com",
+  "kingnet.com",
+  "funp.com",
+  "youthwant.com",
+  "yahoo.com",
+  "cn.yahoo.com",
+  "openfind.com",
+  "timliao.com",
+  "taconet.com",
+  "gigigaga.com",
+  "so-net.net",
+  "don-net.com",
+  "hongkong.com",
+  "asiadog.com",
+  "gotoya.com",
+  "t2t.com",
+  "moninet.com",
+  "myweb.hinet.nethome1kiroro",
+  "udo.idv",
+  "wutun.idv",
+  "centurys.com.tw",
+  "funf",
+  "portal.tw",
+  "pig.tw",
+  "taiwanurl.com",
+  "263.net",
+  "qq.com",
+  "sohu.com",
+  "china.com",
+  "tom.com",
+  "china.com",
+  "netease.com",
+  "backchina.com",
+  "gjj.cc",
+  "qoos.com",
+  "qknet.net"
 ];
+
+/*for (var i = 0; i < entrSites.length; i++) {
+  query_args = query_args.concat(" -site:" + entrSites[i]);
+}*/
 for (var i = 0; i <= post.length - 1; i++) {
   btn[i] = document.createElement("BUTTON");
   btn[i].innerHTML = "more"; // Insert text
@@ -98,22 +178,22 @@ for (var i = 0; i <= post.length - 1; i++) {
   btn[i].id = "btn_" + i;
   (function (i) {
     btn[i].onclick = function () {
-	  console.log(i);
-	  createIFrame(i);
+      console.log(i);
+      createIFrame(i);
     };
   })(i);
   post[i].setAttribute("btn_added", false);
 }
 
 for (var i = 0; i <= innerPost.length - 1; i++) {
-  if (!innerPost[i].innerHTML.includes('class="_52c6"')) {
+  if (!innerPost[i].innerHTML.includes('class="oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 a8c37x1j p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 p8dawk7l"')) {
     btn[i].style.display = "none";
   }
-  if (innerPost[i].innerHTML.includes('class="_52c6"')) {
+  if (innerPost[i].innerHTML.includes('class="oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 a8c37x1j p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 p8dawk7l"')) {
     g[i] = document.createElement('div');
     g[i].id = "website_" + i;
   }
-  innerPost[i].getElementsByClassName("_5pcp _5lel _2jyu _232_")[0].appendChild(btn[i]); // appendChild button to div
+  innerPost[i].getElementsByClassName("oi732d6d ik7dh3pa d2edcug0 hpfvmrgz qv66sw1b c1et5uql a8c37x1j hop8lmos enqfppq2 e9vueds3 j5wam9gi knj5qynh m9osqain hzawbc8m")[0].appendChild(btn[i]); // appendChild button to div
   console.log("button" + i + "created");
   post[i].setAttribute("btn_added", false);
 }
@@ -127,7 +207,32 @@ window.addEventListener("scroll", function (e) {
 
   if (!ticking) {
     window.requestAnimationFrame(function () {
-      post = document.getElementsByClassName("_5pcp _5lel _2jyu _232_");
+      //get posts by using style classes
+      posts = document.getElementsByClassName("rq0escxv l9j0dhe7 du4w35lb hybvsw6c ue3kfks5 pw54ja7n uo3d90p7 l82x9zwi ni8dbmo4 stjgntxs k4urcfbm sbcfpzgs");
+      innerPost = [];
+      //filter it with check whether has content
+      a = 0;
+      for (var r = 0; r < posts.length; r++) {
+        if (posts[r].innerHTML.includes("oi732d6d ik7dh3pa d2edcug0 hpfvmrgz qv66sw1b c1et5uql a8c37x1j hop8lmos enqfppq2 e9vueds3 j5wam9gi knj5qynh m9osqain hzawbc8m")) {
+          innerPost[a] = posts[r]; //if it is a post add it
+          a++;
+        }
+      }
+
+      link_posts = document.getElementsByClassName("oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 a8c37x1j p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 p8dawk7l");
+      linkPost = [];
+      a = 0;
+      for (var i = 0; i < link_posts.length; i++) {
+        //if it is link, then add it
+        if (link_posts[i].tagName == "A") {
+          linkPost[a] = link_posts[i];
+          a++;
+        }
+      }
+      var posts = [];
+      for (var i = 0; i < innerPost.length; i++) {
+        posts[i] = innerPost[i].getElementsByClassName("oi732d6d ik7dh3pa d2edcug0 hpfvmrgz qv66sw1b c1et5uql a8c37x1j hop8lmos enqfppq2 e9vueds3 j5wam9gi knj5qynh m9osqain hzawbc8m")[0];
+      }
       //setTimeout(function () { console.log("appendChilding..."); }, 5000);
       autoappendChild();
       ticking = false;
@@ -136,36 +241,38 @@ window.addEventListener("scroll", function (e) {
   ticking = true;
 });
 
+
+
 function autoappendChild() {	//autoappendChild
 
   for (var j = 0; j <= innerPost.length - 1; j++) {
 
     if (
-      post[j].getAttribute("btn_added") == null
+      posts[j].getAttribute("btn_added") == null
     ) {
       btn[j] = document.createElement("BUTTON");
-	  
+
       btn[j].innerHTML = "more"; // Insert text
       btn[j].setAttribute("class", "btn btn-warning");
 
       btn[j].id = "btn_" + j;
-      if (!innerPost[j].innerHTML.includes('class="_52c6"')) {
+      if (!innerPost[j].innerHTML.includes('class="oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 a8c37x1j p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 p8dawk7l"')) {
         btn[j].style.display = "none";
       }
-      if (innerPost[j].innerHTML.includes('class="_52c6"')) {
+      if (innerPost[j].innerHTML.includes('class="oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 a8c37x1j p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 p8dawk7l"')) {
         g[j] = document.createElement('div');
         g[j].id = "website_" + j;
       }
       (function (j) {
         btn[j].onclick = function () {
           createIFrame(j);
-		  console.log(j);
-		  //console.log("operator_s = "+ operator_s);
+          console.log(j);
+          //console.log("operator_s = "+ operator_s);
         };
       })(j);
-      innerPost[j].getElementsByClassName("_5pcp _5lel _2jyu _232_")[0].appendChild(btn[j]); // appendChild button to div
+      innerPost[j].getElementsByClassName("oi732d6d ik7dh3pa d2edcug0 hpfvmrgz qv66sw1b c1et5uql a8c37x1j hop8lmos enqfppq2 e9vueds3 j5wam9gi knj5qynh m9osqain hzawbc8m")[0].appendChild(btn[j]); // appendChild button to div
       console.log("button" + j + "created");
-      post[j].setAttribute("btn_added", false);
+      posts[j].setAttribute("btn_added", false);
     }
   }
 }
@@ -186,13 +293,15 @@ function areYouInFacebook() {	//areYouInFacebook
   }
 }
 
+
+
 function searchGoogle(googleQuery) {	//searchGoogle
-if(!googleQuery.includes("中天新聞")){
-  var searchUrl = "https://www.google.com/search?q=" + googleQuery.substring(0, 17) + "  -site:facebook.com -site:kknews.cc -site:google.com -site:yahoo.com -site:hinet.net -site:msn.com -site:pchome.com -site:yam.com -site:sina.com -site:cnet.com -site:seed.net. -site:url.com -site:kingnet.com -site:funp.com -site:youthwant.com -site:yahoo.com -site:cn.yahoo.com -site:openfind.com -site:timliao.com -site:taconet.com -site:gigigaga.com -site:so-net.net -site:don-net.com -site:hongkong.com -site:asiadog.com -site:gotoya.com -site:t2t.com -site:moninet.com -site:myweb.hinet.nethome1kiroro -site:udo.idv -site:wutun.idv -site:centurys.com.tw -site:funf -site:portal.tw -site:pig.tw -site:taiwanurl.com -site:263.net -site:qq.com -site:sohu.com -site:china.com -site:tom.com -site:china.com -site:netease.com -site:backchina.com -site:gjj.cc -site:qoos.com -site:qknet.net";
-  }else{
-  	var searchUrl = "https://www.google.com/search?q=" + googleQuery.substring(googleQuery.length-18,googleQuery.length ) + "  -site:facebook.com -site:kknews.cc -site:google.com -site:yahoo.com -site:hinet.net -site:msn.com -site:pchome.com -site:yam.com -site:sina.com -site:cnet.com -site:seed.net. -site:url.com -site:kingnet.com -site:funp.com -site:youthwant.com -site:yahoo.com -site:cn.yahoo.com -site:openfind.com -site:timliao.com -site:taconet.com -site:gigigaga.com -site:so-net.net -site:don-net.com -site:hongkong.com -site:asiadog.com -site:gotoya.com -site:t2t.com -site:moninet.com -site:myweb.hinet.nethome1kiroro -site:udo.idv -site:wutun.idv -site:centurys.com.tw -site:funf -site:portal.tw -site:pig.tw -site:taiwanurl.com -site:263.net -site:qq.com -site:sohu.com -site:china.com -site:tom.com -site:china.com -site:netease.com -site:backchina.com -site:gjj.cc -site:qoos.com -site:qknet.net";
+  if (!googleQuery.includes("中天新聞")) {
+    var searchUrl = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBOXrA4oFgl1SNyxm9sA_vTzaAVYorQDug&cx=9f8b720f1b3abf296&q=" + googleQuery.substring(0, 17);
+  } else {
+    var searchUrl = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBOXrA4oFgl1SNyxm9sA_vTzaAVYorQDug&cx=9f8b720f1b3abf296&q=" + googleQuery.substring(googleQuery.length - 18, googleQuery.length);
   }
-  
+
   return searchUrl;
 }
 
@@ -201,28 +310,29 @@ function createElementFromHTML(htmlString) {	//createElementFromHTML
   div.innerHTML = htmlString.trim();
 
   // Change this to div.childNodes to support multiple top-level nodes
-  return div.firstChild; 
+  return div.firstChild;
 }
 
+
 function loadFileToElement(filename, operator_n) {
-
-
 
   $(document).ready(function () {
     $.ajax({
       type: "GET",
       url: "https://morning-woodland-98584.herokuapp.com/" + filename,
-      async: false,
+      async: true,
       headers: {
         "x-requested-with": "xhr"
       },
-      mode: 'cors',
+      mode: 'json',
       cache: 'default',
       success: function (text) {
-        g[operator_n].innerHTML = text;
+        g[operator_n] = text;
       }
     });
   });
+
+
 }
 
 
@@ -239,66 +349,78 @@ function createIFrame(operator_k) {
 
 
   var checkTarget = document.getElementById("iframe_" + operator_k); //some error 
-  loadFileToElement(searchGoogle(innerPost[operator_k].getElementsByClassName("_6m3 _--6")[0].childNodes[1].childNodes[0].innerText), operator_k);
-  var search_result = g[operator_k].getElementsByClassName("LC20lb");
-  var icos = [document.createElement("img"), document.createElement("img"), document.createElement("img"), document.createElement("img"), document.createElement("img")]; //old icon functions
-  for (var operator_q = 0; operator_q < search_result.length; operator_q++) {	//filter the entrance websites
-    for (var operator_p = 0; operator_p < entrSites.length; operator_p++) {
-      if (search_result[operator_q].innerHTML.includes(entrSites[operator_p])) {
-        search_result[operator_q] = search_result[operator_q + 1];
-      }
-    }
-  }
-  
-	var getLocation = function(href) {
-		var l = document.createElement("a");
-		l.href = href;
-		return l;
+  loadFileToElement(searchGoogle(innerPost[operator_k].getElementsByClassName("qzhwtbm6 knvmm38d")[3].innerText), operator_k);
+  var delay = function(s){
+    return new Promise(function(resolve,reject){
+     setTimeout(resolve,s); 
+    });
   };
-  
- 
-  for (var operator_r = 0; operator_r < 5; operator_r++) {	//set the icons on the search_results
-  
-	
-    var string1 = search_result[operator_r].parentNode.href;
-    var string2 = "http://"+ getLocation(string1).hostname;
-    //string2.replace(window.location.href, "");
-	string2 = string2 + "/favicon.ico";
-    icos[operator_r].src = string2;
-	
-	
-    icos[operator_r].width = 12;	//set width as 12
-    icos[operator_r].height = 12;	//set height as 12
+  delay().then(function(){
+    return delay(1000); // 延遲ㄧ秒
+  }).then(function(){
+  var search_result = [];
+  for (var i = 0; i < 10; i++) {
+    var result = document.createElement("div");
+    result.id = "results-" + i;
+    var item = g[operator_k].items[i];
+    // in production code, item.htmlTitle should have the HTML entities escaped.
+
+    var link = document.createElement('a');
+    link.href = item.link;
+    link.innerText = item.title;
+    result.appendChild(link);
+    search_result[i] = result;
   }
- 
+
+  //var icos = [document.createElement("img"), document.createElement("img"), document.createElement("img"), document.createElement("img"), document.createElement("img")]; //old icon functions
+  var icos = [];
+  for (var i = 0; i < 10; i++) {
+    icos.push(document.createElement("img"));
+  }
+  var getLocation = function (href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+  };
+
+
+  for (var operator_r = 0; operator_r < 10; operator_r++) {	//set the icons on the search_results
+
+
+    var string1 = "http://i.olsh.me//icon?url=" + getLocation(search_result[operator_r].getElementsByTagName("A")[0].href).hostname + "&size=80..120..200";
+    icos[operator_r].src = string1;
+
+
+    icos[operator_r].width = 24;	//set width as 24
+    icos[operator_r].height = 24;	//set height as 24
+    icos[operator_r].style.position = "relative";
+  }
+
 
   var tr_ele = [];
-
-  tr_ele[0] = document.createElement("tr");
-  tr_ele[1] = document.createElement("tr");
-  tr_ele[2] = document.createElement("tr");
-  tr_ele[3] = document.createElement("tr");
-  tr_ele[4] = document.createElement("tr");
+  for (var i = 0; i < 10; i++)
+    tr_ele[i] = document.createElement("tr");
 
   if (checkTarget == null) {
     //if (search_result[operator_k] != null) {
-      ifrm[operator_k] = document.createElement("div");
-      for (var operator_m = 0; operator_m < search_result.length; operator_m++) {
-	tr_ele[operator_m].appendChild(icos[operator_m]);
-        tr_ele[operator_m].appendChild(search_result[operator_m].parentNode);
-        tbody[operator_k].appendChild(tr_ele[operator_m]);
-        table[operator_k].appendChild(tbody[operator_k]);
-      }
-      ifrm[operator_k].appendChild(table[operator_k]);
-      ifrm[operator_k].id = "iframe_" + operator_k;
-      innerPost[operator_k].getElementsByClassName("_5pcp _5lel _2jyu _232_")[0].appendChild(ifrm[operator_k]);
-      ifrm[operator_k].setAttribute("style", "display: block");
-      console.log("iFrame" + operator_k + "created");
+    ifrm[operator_k] = document.createElement("div");
+    for (var operator_m = 0; operator_m < search_result.length; operator_m++) {
+      search_result[operator_m].insertBefore(icos[operator_m], search_result[operator_m].firstChild);
+      //tr_ele[operator_m].appendChild(icos[operator_m]);
+      tr_ele[operator_m].appendChild(search_result[operator_m]);
+      tbody[operator_k].appendChild(tr_ele[operator_m]);
+      table[operator_k].appendChild(tbody[operator_k]);
+    }
+    ifrm[operator_k].appendChild(table[operator_k]);
+    ifrm[operator_k].id = "iframe_" + operator_k;
+    innerPost[operator_k].getElementsByClassName("oi732d6d ik7dh3pa d2edcug0 hpfvmrgz qv66sw1b c1et5uql a8c37x1j hop8lmos enqfppq2 e9vueds3 j5wam9gi knj5qynh m9osqain hzawbc8m")[0].appendChild(ifrm[operator_k]);
+    ifrm[operator_k].setAttribute("style", "display: block");
+    console.log("iFrame" + operator_k + "created");
     //}
   } else {
     console.log("iFrame" + operator_k + "already exists");
     toggleOnOff(operator_k);
   }
-  return {search_result: search_result, icos: icos};
-  
+  return { search_result: search_result, icos: icos };
+});
 }
