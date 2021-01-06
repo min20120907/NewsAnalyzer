9ed07202d166d859ebfbf16794bd5d8c14f0dadf
Cangyu Lin <jefflin.je598@gmail.com>
2021-01-06 15:41:29 +0800

    fix the problem that the words out of the dictionary list

--- news_kw_extract.py
+++ news_kw_extract.py
@@ -5,14 +5,14 @@ from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route('/extract', methods = ['GET'])
-jieba.set_dictionary("https://github.com/fxsjy/jieba/raw/master/extra_dict/dict.txt.big")
 def extract():
+    jieba.set_dictionary("https://github.com/fxsjy/jieba/raw/master/extra_dict/dict.txt.big")
     model = KeyBERT('LaBSE')
     title = request.args.get('title')
     keywords = []
     lang = TextBlob(title)
     if(lang.detect_language()=='zh-CN' or lang.detect_language()=='zh-TW'):
-        splitted_title = " ".join(jieba.cut(title))
+        splitted_title = " ".join(jieba.cut(title, HMM=False))
         print("The Title is:\n",splitted_title)
         keywords = model.extract_keywords(splitted_title,stop_words=[',' , '，', '.', '。', '?', '？', '!', '！', '#', '＃', '/', '／', ':', '：', '(', '（', ')', '）', '『', '「', '【', '〖', '［', '』', '」', '】', '〗', '］', '[', ']', '-', '_', '＿', '——', '－', '-', '−', '我', '你','妳', '他', '她', '它', '祂', '是', '的', '了', '呢', '嗎', '問', '問題', '問卷', '什麼', '新聞', '分享', '討論', '這個', '那個', '哪個', '最', '爆', '傳', '驚魂', '這項', '曝', '這招', '那招', '什麼', '驚', '推']) 
     else:
