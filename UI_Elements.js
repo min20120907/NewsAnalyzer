9d2e65e5c8c0ecd98ce80758ea1c8e97f5c01a67
Cangyu Lin <jefflin.je598@gmail.com>
2021-05-28 14:22:17 +0800

    add object orienting initialization

---
+++ UI_Elements.js
@@ -0,0 +1,23 @@
+class UI_Elements {
+    linkposts = new Array();    
+    class_linkpost = "rq0escxv l9j0dhe7 du4w35lb j83agx80 pfnyh3mw i1fnvgqd bp9cbjyn owycx6da btwxx1t3 b5q2rw42 lq239pai f10w8fjw hv4rvrfc dati1w0a pybr56ya";
+    class_post="rq0escxv l9j0dhe7 du4w35lb hybvsw6c io0zqebd m5lcvass fbipl8qg nwvqtn77 k4urcfbm ni8dbmo4 stjgntxs sbcfpzgs";
+    class_header="pybr56ya dati1w0a hv4rvrfc n851cfcs btwxx1t3 j83agx80 ll8tlv6m";
+    class_link="l9j0dhe7";
+    constructor(class_linkpost, class_post){
+
+    }
+    // The function to extract the class names into the elements array
+    fetch_elements(params) {
+
+    }
+    // The function to add the button inside the post
+    append_button(){
+
+    }
+    // The function to add the frame that contain the results into the post
+    append_result(){
+        
+    }
+
+}
--- resultFrame.js
+++ resultFrame.js
@@ -1,8 +1,10 @@
 class resultFrame {
-    constructor(icon_list, title_list, URL_list) {
+    
+    constructor(title,icon_list, title_list, URL_list) {
         this.icon_list = icon_list;
         this.title_list = title_list;
         this.URL_list = URL_list;
+        this.title = title;
     }
-    
+
 }
