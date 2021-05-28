1e8fd22e479720fa423351a20d5da89551d2018c
Cangyu Lin <jefflin.je598@gmail.com>
2021-05-28 13:53:51 +0800

    reorganize the files

---
+++ link_post.js
@@ -0,0 +1,7 @@
+class linkpost{
+    keywords = new Array(3);
+    constructor(URL, title){
+        this.URL = URL;
+        this.title = title;
+    }
+}
---
+++ resultFrame.js
@@ -0,0 +1,8 @@
+class resultFrame {
+    constructor(icon_list, title_list, URL_list) {
+        this.icon_list = icon_list;
+        this.title_list = title_list;
+        this.URL_list = URL_list;
+    }
+    
+}
