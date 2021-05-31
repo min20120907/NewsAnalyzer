4fbb9e2c44d015535fe26dbb3d15df9b37afa7ff
lusterofgem <lusterofgem@gmail.com>
2021-06-01 00:17:22 +0800

    Use XMLHttpRequest for blacklist.js and whitelist.js, but didn't test yet.

--- blacklist.js
+++ blacklist.js
@@ -1,13 +1,27 @@
 let blacklist = class {
-    static isBlackList(URL) {
-        /* IE only */
-        let fso = ActiveXObject(Scripting.FileSystemObject);
-        let whiteListData = fso.createtextfile("blacklist.txt",1,false);
-        while(!whiteListData.AtEndOfStream) {
-            if(URL == whiteListData.ReadLine()) {
-                return true;
+    // static inBlackList(URL) {
+    //     const BLACK_LIST_URL = "https://raw.githubusercontent.com/lusterofgem/snakeGame/master/README.md";
+    //     fetch(BLACK_LIST_URL)
+    //         .then((responce) => {console.log(responce.text());})
+    // }
+    
+    static inBlackList(URL) {
+        if(getBlackList().include(URL)) {
+            return true;
+        }
+        else {
+            return true;
+        }
+    }
+
+    static getBlackList() {
+        let request = new XMLHttpRequest();
+        request.onreadystatechange=function() {
+            if(request.readyState==4 && request.status==200) {
+                return request.responseText;
             }
         }
-        return false;
+        request.open("GET", "https://raw.githubusercontent.com/min20120907/NewsAnalyzer/master/blacklist.txt", false);
+        request.send();
     }
 }
--- whitelist.js
+++ whitelist.js
@@ -1,33 +1,21 @@
 let whitelist = class {
-    static isAllow(URL) {
-        /* IE only */
-        let fso = ActiveXObject(Scripting.FileSystemObject);
-        let whiteListData = fso.createtextfile("whitelist.txt",1,false);
-        while(!whiteListData.AtEndOfStream) {
-            if(URL == whiteListData.ReadLine()) {
-                return true;
+    static inWhiteList(URL) {
+        if(getWhiteList().include(URL)) {
+            return true;
+        }
+        else {
+            return true;
+        }
+    }
+
+    static getWhiteList() {
+        let request = new XMLHttpRequest();
+        request.onreadystatechange=function() {
+            if(request.readyState==4 && request.status==200) {
+                return request.responseText;
             }
         }
-        return false;
-
-        /* Node.js */
-        // const fs = require('fs');
-        // const readline = require('readline')
-        // let inputStream = fs.createReadStream('whitelist.txt');
-        // let lineReader = readline.createInterface({input:inputStream});
-        // lineReader.on('line', function(line) {
-        //     console.log(line);
-        // });
-
-        /* read local file */
-        // let rawFile = new XMLHttpRequest();
-        // rawFile.open("GET", "./whitelist.txt", false);
-        // rawFile.onreadystatechange = function() {
-        //     if(rawFile.readyState === 4) {
-        //         if(rawFile.status === 200 || rawFile.status == 0) {
-        //             console.log(rawFile.responceText);
-        //         }
-        //     }
-        // }
+        request.open("GET", "https://raw.githubusercontent.com/min20120907/NewsAnalyzer/master/whitelist.txt", false);
+        request.send();
     }
 }
