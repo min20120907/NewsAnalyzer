ad44dc1aa51b591c3289f135c28bc9e75bd63e14
Cangyu Lin <jefflin.je598@gmail.com>
2021-05-31 22:03:29 +0800

    undo it back

--- UI_Elements.js
+++ UI_Elements.js
@@ -47,8 +47,7 @@ let UI_Elements = class {
     // The function to export the link
     getLink(i) {
         this.links[i] = (new linkpost(
-            document.querySelectorAll("a." + this.queryOf(class_linkaddr))[i].origin+
-            document.querySelectorAll("a." + this.queryOf(class_linkaddr))[i].pathname,
+            document.querySelectorAll("a." + this.queryOf(class_linkaddr))[i].href,
             document.querySelectorAll("a." + this.queryOf(class_linkaddr))[i]
                 .querySelectorAll("span." + this.queryOf(class_linktitle))[1].innerText
         ));
