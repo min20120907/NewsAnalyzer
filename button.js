36f7c9b2712ed5577bb478679f6a08d7285d4109
Cangyu Lin <jefflin.je598@gmail.com>
2021-05-31 17:40:49 +0800

    fix button class

--- button.js
+++ button.js
@@ -1,25 +1,32 @@
-let button = new class{
-    
-    constructor(ID, text, className){
-        // DOM
-        this.dom = document.createElement("BUTTON");
-        this.dom.setAttribute("class", className);
-        this.dom.innerText = text;
-        this.dom.id = "btn_" + ID;
-        // onclick function
-        (function (ID) {
-            this.dom.onclick = function () {
-              console.log(ID);
-              // createIFrame(i);
-            };
-          })(ID);
+let button = class {
+
+  constructor(ID, text, className) {
+    this.ID=ID;
+    this.text = text;
+    this.className = className;
+    if(document.getElementById("btn_"+this.ID)==null)
+      this.constructButton();
+  }
+  constructButton(){
+    // DOM
+    this.dom = document.createElement("BUTTON");
+    this.dom.setAttribute("class", this.className);
+    this.dom.innerText = this.text;
+    this.dom.id = "btn_" + this.ID;
+    // onclick function
+    (function (ID, btn) {
+      btn.dom.onclick = function () {
+        console.log(ID);
+        // createIFrame(i);
+      };
+    })(this.ID, this);
+  }
+  // toggle the button on off state
+  toggleOnOff(element) {	//toggleOnOff
+    if (element.style.display == "none") {
+      element.setAttribute("style", "display: block");
+    } else {
+      element.setAttribute("style", "display: none");
     }
-    // toggle the button on off state
-    toggleOnOff(element) {	//toggleOnOff
-        if (element.style.display == "none") {
-          element.setAttribute("style", "display: block");
-        } else {
-          element.setAttribute("style", "display: none");
-        }
-      }
+  }
 }
