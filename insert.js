f977537ef0277c039eb8de0d3863f0c72d30caf7
Cangyu Lin <jefflin.je598@gmail.com>
2021-05-31 14:19:48 +0800

    add delay function

--- insert.js
+++ insert.js
@@ -1,17 +1,22 @@
 // Loading event listener
 window.addEventListener("load", UI_Elements.areYouInFacebook);
 let UI = new UI_Elements();
-
+// delay function
+let delay = function (s) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(resolve, s);
+    });
+};
 let num = UI.linkposts.length;
 let ticking = false;
 let last_known_scroll_position = 0;
 // Scrolling event listener
 window.addEventListener("scroll", function (e) {
-  last_known_scroll_position = window.scrollY;
-  if (!ticking) {
-    window.requestAnimationFrame(function () {
-      ticking = false;
-    });
-  }
-  ticking = true;
+    last_known_scroll_position = window.scrollY;
+    if (!ticking) {
+        window.requestAnimationFrame(function () {
+            ticking = false;
+        });
+    }
+    ticking = true;
 });
