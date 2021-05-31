ab0c06cae7c337de68dc522b47996d7d82028e52
Cangyu Lin <jefflin.je598@gmail.com>
2021-05-31 21:39:49 +0800

    change append to inplace

--- UI_Elements.js
+++ UI_Elements.js
@@ -45,7 +45,7 @@ let UI_Elements = class {
     }
     // The function to export the link
     getLink(i) {
-        this.links.push(new linkpost(
+        this.links[i](new linkpost(
             document.querySelectorAll("a." + this.queryOf(class_linkaddr))[i].href,
             document.querySelectorAll("a." + this.queryOf(class_linkaddr))[i]
                 .querySelectorAll("span." + this.queryOf(class_linktitle))[1].innerText
