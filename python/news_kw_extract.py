c0d704ce6a1c4f3169666337ef7ab5582e581c90
Cangyu Lin <jefflin.je598@gmail.com>
2021-06-04 16:23:32 +0800

    optimize GPU performance

--- python/news_kw_extract.py
+++ python/news_kw_extract.py
@@ -10,34 +10,22 @@ cors = CORS(app, resources={r"/api/*": {"origins": ["https://www.facebook.com"]}
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+model = KeyBERT('LaBSE')
+jieba.set_dictionary("dict.txt.big")
 @app.route('/extract', methods = ['GET'])
 @cross_origin()
 def extract():
     kw=[]
-    jieba.set_dictionary("dict.txt.big")
-    model = KeyBERT('LaBSE')
     title = request.args.get('title')
     keywords = []
-    lang = TextBlob(title)
-    if(lang.detect_language()=='zh-CN' or lang.detect_language()=='zh-TW'):
-        splitted_title = " ".join(jieba.cut(title, HMM=False))
-        print("The Title is:\n",splitted_title)
-        keywords = model.extract_keywords(splitted_title,stop_words=[',' , '，', '.', '。', '?', '？', '!', '！', '#', '＃', '/', '／', ':', '：', '(', '（', ')', '）', '『', '「', '【', '〖', '［', '』', '」', '】', '〗', '］', '[', ']', '-', '_', '＿', '——', '－', '-', '−', '我', '你','妳', '他', '她', '它', '祂', '是', '的', '了', '呢', '嗎', '問', '問題', '問卷', '什麼', '新聞', '分享', '討論', '這個', '那個', '哪個', '最', '爆', '傳', '驚魂', '這項', '曝', '這招', '那招', '什麼', '驚', '推']) 
-        for a in keywords:
-            kw.append(a[0])
+    splitted_title = " ".join(jieba.cut(title, HMM=False))
+    print("The Title is:\n",splitted_title)
+    keywords = model.extract_keywords(splitted_title,stop_words=[',' , '，', '.', '。', '?', '？', '!', '！', '#', '＃', '/', '／', ':', '：', '(', '（', ')', '）', '『', '「', '【', '〖', '［', '』', '」', '】', '〗', '］', '[', ']', '-', '_', '＿', '——', '－', '-', '−', '我', '你','妳', '他', '她', '它', '祂', '是', '的', '了', '呢', '嗎', '問', '問題', '問卷', '什麼', '新聞', '分享', '討論', '這個', '那個', '哪個', '最', '爆', '傳', '驚魂', '這項', '曝', '這招', '那招', '什麼', '驚', '推']) 
+    for a in keywords:
+        kw.append(a[0])
 
-        # keywords = model.extract_keywords(splitted_title)
-    else:
-        keywords = model.extract_keywords(title,stop_words='english')
-        for a in keywords:
-            kw.append(a[0])
     del keywords
     response = "//".join(kw)
-    del kw
-    del model
-    del title
-    del lang
-
     return response
 
 if __name__ == '__main__':
