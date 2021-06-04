761caa7e978429fa75b040f57a25a392d99b42e2
Cangyu Lin <jefflin.je598@gmail.com>
2021-06-04 17:23:02 +0800

    fix hostname problem

--- icon.js
+++ icon.js
@@ -2,7 +2,7 @@ let icon = class{
     constructor(search_result) {
         this.dom = document.createElement("img");
         this.dom.src = "http://i.olsh.me/icon?url=" +
-                         search_result.link.hostname + 
+                         new URL(search_result.link).hostname + 
                          "&size=80..120..200";
         this.dom.width = 24;	//set width as 24
         this.dom.height = 24;	//set height as 24
