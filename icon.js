37ce502e8812e9b1322b52675795c79934e961f1
Cangyu Lin <jefflin.je598@gmail.com>
2021-06-06 01:15:50 +0800

    update icon server

--- icon.js
+++ icon.js
@@ -1,9 +1,8 @@
 let icon = class{
     constructor(search_result) {
         this.dom = document.createElement("img");
-        this.dom.src = "http://" +
-                         new URL(search_result).hostname + 
-                         "/favicon.ico";
+        this.dom.src = "https://www.google.com/s2/favicons?sz=64&domain_url=" +
+                         new URL(search_result).hostname;
         this.dom.width = 24;	//set width as 24
         this.dom.height = 24;	//set height as 24
         this.dom.style.position = "relative";
