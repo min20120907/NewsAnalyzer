a845e50b2a8d84ec0a8cd0d74d91b1ae19a9d5d1
Teng <peitengtsai@gmail.com>
2022-11-07 14:32:04 +0800

    測試是否成功連線到google_news

---
+++ news_crawler/google_news.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+import re
+from tldextract import tldextract
+#設定fake-useragent
+#假的user-agent,產生 headers
+ua=UserAgent()
+usar=ua.random #產生header 字串
+headers={'user-agent':usar}
+url='https://news.google.com/topstories?hl=zh-TW&gl=TW&ceid=TW:zh-Hant'
+
+htmlfile=requests.get(url,headers=headers,timeout=3)#他這邊請求website後,得到一個物件
+
+if htmlfile.status_code==requests.codes.ok:
+    print("成功連線到google news")
+htmlfile.encoding='utf-8'
+#開始使用bs4 解析
+objsoup=BeautifulSoup(htmlfile.text,"lxml")
