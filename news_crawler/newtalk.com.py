3e6b662c6380b83cdc942139c5bd590be5c17f19
tengsnake <peitengtsai@gmail.com>
2022-11-11 14:25:44 +0800

    simplify the code

--- news_crawler/newtalk.com.py
+++ news_crawler/newtalk.com.py
@@ -5,10 +5,8 @@ res=requests.get(url2)
 if res.status_code==requests.codes.ok:
     print('ok')
 objsoup=BeautifulSoup(res.text,'lxml')
-# Find all of the text between paragraph tags and strip out the html
 title=objsoup.find('h1',{"class":"content_title"})
 print("新聞標題: ",title.text)
-VALID_TAGS = ['p']
 print("文章內容: ")
 contents=objsoup.find('div',{"id":"news_content"}).find_all('p')
 for content in contents:
