8c8e2aa90a15f6f2a4ad10e949dad4ca490137b8
Teng <peitengtsai@gmail.com>
2022-11-15 13:52:36 +0800

    update

--- news_crawler/news.py
+++ news_crawler/news.py
@@ -96,7 +96,6 @@ def domain_check(domain,news_url):
                 print('ltn ok')
             objsoup=BeautifulSoup(res.text,'lxml')
             title=objsoup.find('h1')
-            ban_set={"請繼續往下閱讀...","不用抽 不用搶 現在用APP看新聞 保證天天中獎"}
             print("新聞標題: ",title.text)
             contents=objsoup.find('div',{"class":"text boxTitle boxText"}).find_all('p')
             print("文章內容: ")
