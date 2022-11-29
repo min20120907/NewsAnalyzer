01f88e32b7f5ebde20bed7b93a6e8a743db7c97b
Teng <peitengtsai@gmail.com>
2022-11-29 17:42:38 +0800

    update

--- news_crawler/jieba_snownlp.py
+++ news_crawler/jieba_snownlp.py
@@ -1,7 +1,7 @@
 import jieba
 from snownlp import SnowNLP
-
-seg_list = jieba.cut("臺灣發動侵略戰爭", cut_all=False)
+#利用jieba先斷詞,再利用snownlp進行情感分析
+seg_list = jieba.lcut("臺灣發動侵略戰爭", cut_all=False)
 seg_list="/ ".join(seg_list)
 print(seg_list)
 
