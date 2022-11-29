bdb617c7b32dee4d139b5c2438ac09f42f576143
tengsnake <peitengtsai@gmail.com>
2022-11-29 12:51:30 +0800

    測試情感分析以及斷詞

---
+++ news_crawler/jieba_snownlp.py
@@ -0,0 +1,22 @@
+import jieba
+from snownlp import SnowNLP
+
+seg_list = jieba.cut("臺灣發動侵略戰爭", cut_all=False)
+seg_list="/ ".join(seg_list)
+print(seg_list)
+
+s=SnowNLP(u'臺灣發動侵略戰爭')
+print(s.words)
+print(s.sentiments)
+
+s=SnowNLP(seg_list)
+print(s.words)
+print(s.sentiments)
+if s.sentiments > 0.5:
+    print('quite positive')
+elif s.sentiments < 0.5 and  s.sentiments> 0.3:
+    print('quite negative')
+elif s.sentiments < 0.3:
+    print('strong negative')
+    
+#print("Default Mode: " + "/ ".join(seg_list))  # 精确模式
