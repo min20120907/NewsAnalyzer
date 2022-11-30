dde26494fef84e509d61d14161986aa118a9398b
Cangyu Lin <jefflin.je598@gmail.com>
2022-11-30 22:46:18 +0800

    tweak a scope a bit

--- news_crawler/news6.py
+++ news_crawler/news6.py
@@ -7,41 +7,101 @@ import pymysql
 import jieba
 import jieba.analyse
 from snownlp import SnowNLP
+from keybert import KeyBERT
+import jieba
+from textblob import TextBlob
+from flask import Flask, redirect, url_for, request
 # 設定fake-useragent
 # 假的user-agent,產生 headers
 ua=UserAgent()
 usar=ua.random #產生header 字串
+
 headers={'user-agent':usar}
-# if key words are 烏克蘭 戰爭 俄羅斯
-keywords="烏克蘭 戰爭 俄羅斯"
-same_url='https://news.google.com/search?q='+keywords+'&hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant'
+app = Flask(__name__)
 
-htmlfile=requests.get(same_url,headers=headers,timeout=5)
 
-if htmlfile.status_code==requests.codes.ok:
-    print("成功連線到google news with string")
-htmlfile.encoding='utf-8'
+@app.route('/extract', methods = ['GET'])
+def extract():
+    model = KeyBERT('LaBSE')
+    title = request.args.get('title')
+    splitted_title = " ".join(jieba.cut(title))
+    keywords = model.extract_keywords(splitted_title,stop_words=[',' , '，', '.', '。', '?', '？', '!', '！', '#', '＃', '/', '／', ':', '：', '(', '（', ')', '）', '『', '「', '【', '〖', '［', '』', '」', '】', '〗', '］', '[', ']', '-', '_', '＿', '——', '－', '-', '−', '我', '你','妳', '他', '她', '它', '祂', '是', '的', '了', '呢', '嗎', '問', '問題', '問卷', '什麼', '新聞', '分享', '討論', '這個', '那個', '哪個', '最', '爆', '傳', '驚魂', '這項', '曝', '這招', '那招', '什麼', '驚', '推']) 
+    # if key words are 烏克蘭 戰爭 俄羅斯
+    keywords_str=" ".join([i[0] for i in keywords])
+    same_url='https://news.google.com/search?q='+keywords_str+'&hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant'
 
-# ban strings
-ban_set={"© 2022 BBC. BBC對外部網站內容不負責任。 閱讀了解我們對待外部鏈接的做法。","圖像來源，Reuters"
-,"中時新聞網對留言系統使用者發布的文字、圖片或檔案保有片面修改或移除的權利。當使用者使用本網站留言服務時，表示已詳細閱讀並完全了解，且同意配合下述規定：","違反上述規定者，中時新聞網有權刪除留言，或者直接封鎖帳號！請使用者在發言前，務必先閱讀留言板規則，謝謝配合。"
-,"本網站之文字","本網站之文字、圖片及影音，非經授權，不得轉載、公開播送或公開傳輸及利用。"
-,"省錢大作戰！超夯優惠等你GO"
-,"請繼續往下閱讀...","不用抽 不用搶 現在用APP看新聞 保證天天中獎"
-,"Photo Credit:","每月一杯咖啡的金額，支持優質觀點的誕生，享有更好的閱讀體驗。","本文經《BBC News 中文》授權轉載，原文發表於此"
-,"更多 TVBS 報導","更多相關新聞,'相關新聞影音"
-,'圖／TVBS'
-,'圖像來源，NCA'
-,'原始連結'
-,'點我看更多華視新聞＞＞＞'
-,'圖像來源，Getty Images'
-,'相關新聞影音'
-,'[啟動LINE推播] 每日重大新聞通知'
-,'下載法廣應用程序跟蹤國際時事'}
-# break string
-break_set={'點我看更多華視新聞＞＞＞','更多風傳媒報導','更多 TVBS 報導'}
+    htmlfile=requests.get(same_url,headers=headers,timeout=5)
 
-# 新聞標題以及內文斷詞,並回傳positive還是negative
+    if htmlfile.status_code==requests.codes.ok:
+        print("成功連線到google news with string")
+    htmlfile.encoding='utf-8'
+
+    # ban strings
+    ban_set={"© 2022 BBC. BBC對外部網站內容不負責任。 閱讀了解我們對待外部鏈接的做法。","圖像來源，Reuters"
+    ,"中時新聞網對留言系統使用者發布的文字、圖片或檔案保有片面修改或移除的權利。當使用者使用本網站留言服務時，表示已詳細閱讀並完全了解，且同意配合下述規定：","違反上述規定者，中時新聞網有權刪除留言，或者直接封鎖帳號！請使用者在發言前，務必先閱讀留言板規則，謝謝配合。"
+    ,"本網站之文字","本網站之文字、圖片及影音，非經授權，不得轉載、公開播送或公開傳輸及利用。"
+    ,"省錢大作戰！超夯優惠等你GO"
+    ,"請繼續往下閱讀...","不用抽 不用搶 現在用APP看新聞 保證天天中獎"
+    ,"Photo Credit:","每月一杯咖啡的金額，支持優質觀點的誕生，享有更好的閱讀體驗。","本文經《BBC News 中文》授權轉載，原文發表於此"
+    ,"更多 TVBS 報導","更多相關新聞,'相關新聞影音"
+    ,'圖／TVBS'
+    ,'圖像來源，NCA'
+    ,'原始連結'
+    ,'點我看更多華視新聞＞＞＞'
+    ,'圖像來源，Getty Images'
+    ,'相關新聞影音'
+    ,'[啟動LINE推播] 每日重大新聞通知'
+    ,'下載法廣應用程序跟蹤國際時事'}
+    # break string
+    break_set={'點我看更多華視新聞＞＞＞','更多風傳媒報導','更多 TVBS 報導'}
+
+    # 新聞標題以及內文斷詞,並回傳positive還是negative
+    
+
+    # 開始使用bs4解析
+    objsoup=BeautifulSoup(htmlfile.text,"lxml")
+
+    #取得objsoup所有的文字
+    # print(objsoup.get_text())
+
+    # 找到所有google新聞的link
+    url_link_list=[]
+    h3_all_links=objsoup.find_all('h3',{"class":"ipQwMb ekueJc RD0gLb"})
+    for counter,h3_all_link in enumerate(h3_all_links):
+        #print(h3_all_link.text)
+        url_link_list.append(h3_all_link.find('a')['href'])
+        if counter>=11:
+            break
+
+    # 把link拿出來看看
+    # print(url_link_list)
+    url_link_list_remove_dot=[]
+    for link in url_link_list:
+        url_link_list_remove_dot.append(link.replace('./','',1))
+
+    # 解決短網址問題
+    def shortlink_converter(url):
+        resp = requests.get(url)
+        return resp.url
+
+    # 連到多家新聞媒體
+    for link in url_link_list_remove_dot:
+        url='https://news.google.com/'+str(link)
+        original_url=shortlink_converter(url)
+        res=requests.get(original_url,headers=headers,timeout=10) 
+        # if res.status_code==requests.codes.ok:
+        #    print('ok')
+        
+        # 判斷連到的是哪個domain,以抓去特定媒體的內文tag
+        news_url=res.request.url #特定新聞媒體的url
+        # 解析domain    
+        tld_result = tldextract.extract(news_url)
+        domain = '{}.{}'.format(tld_result.domain, tld_result.suffix)
+        domain_check(domain,news_url)
+    return "Uploading the database"
+ 
+ 
+
 # 關鍵字榨取與情感分析
 def kw(title,content_str):
     # 關鍵字榨取
@@ -521,44 +581,6 @@ def domain_check(domain,news_url):
             insert_data(title.text,content_str,news_url,news_title_kw,news_content_kw,sentiments_analysis) 
         case _:
             return "url missing!"
+if __name__ == '__main__':
+    app.run(debug = True)
 
-# 開始使用bs4解析
-objsoup=BeautifulSoup(htmlfile.text,"lxml")
-
-#取得objsoup所有的文字
-# print(objsoup.get_text())
-
-# 找到所有google新聞的link
-url_link_list=[]
-h3_all_links=objsoup.find_all('h3',{"class":"ipQwMb ekueJc RD0gLb"})
-for counter,h3_all_link in enumerate(h3_all_links):
-    #print(h3_all_link.text)
-    url_link_list.append(h3_all_link.find('a')['href'])
-    if counter>=11:
-        break
-
-# 把link拿出來看看
-# print(url_link_list)
-url_link_list_remove_dot=[]
-for link in url_link_list:
-    url_link_list_remove_dot.append(link.replace('./','',1))
-
-# 解決短網址問題
-def shortlink_converter(url):
-    resp = requests.get(url)
-    return resp.url
-
-# 連到多家新聞媒體
-for link in url_link_list_remove_dot:
-    url='https://news.google.com/'+str(link)
-    original_url=shortlink_converter(url)
-    res=requests.get(original_url,headers=headers,timeout=10) 
-    # if res.status_code==requests.codes.ok:
-    #    print('ok')
-    
-    # 判斷連到的是哪個domain,以抓去特定媒體的內文tag
-    news_url=res.request.url #特定新聞媒體的url
-    # 解析domain    
-    tld_result = tldextract.extract(news_url)
-    domain = '{}.{}'.format(tld_result.domain, tld_result.suffix)
-    domain_check(domain,news_url)
