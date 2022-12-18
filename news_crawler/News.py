6453f63d4e4480211aa2b1134bd12902f0bfd1a6
Cangyu <jefflin.je598@gmail.com>
2022-12-18 11:41:28 +0800

    merge fcc results

--- news_crawler/News.py
+++ news_crawler/News.py
@@ -51,6 +51,9 @@ class News:
         self.sentiment_analysis = []
         # The domain of the news
         self.domain = []
+        # fcc result
+        self.fcc_results = []
+
         # 開始使用bs4解析
         objsoup = BeautifulSoup(self.htmlfile.text, "lxml")
 
@@ -97,6 +100,69 @@ class News:
         for thread in t:
             thread.join()
         
+    def fcc_search(news_title_kw):
+        # 以下開始必須進行fcc的查詢
+        # 藉由google news來輔助事實查核
+        # url='https://tfc-taiwan.org.tw/articles/8530'
+        # 制約網站域名確保搜尋結果乾淨
+        fcc_result=''
+        site_restrict=' site:https://tfc-taiwan.org.tw/'
+        query_url='https://news.google.com/search?q='+news_title_kw+site_restrict+'&hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant'
+        #print(query_url)
+        htmlfile=requests.get(query_url,headers=headers,timeout=5)
+        if htmlfile.status_code==requests.codes.ok:
+            print("成功連線到google news! 帶著欲查詢字串")
+        htmlfile.encoding='utf-8'
+
+        # 對唯一的目標網站進行連接
+        try:
+            #開始使用bs4 解析
+            url_link_list=[]
+            objsoup=BeautifulSoup(htmlfile.text,"lxml")
+            link=objsoup.find_all('div',{"class":"xrnccd"})
+            for lk in link:
+                url_link_list.append(lk.find('a')['href'])
+            #print(url_link_list)
+
+            url_link_list_remove_dot=[]
+            for link in url_link_list:
+                url_link_list_remove_dot.append(link.replace('./','',1))
+            #print(url_link_list_remove_dot)
+
+            # 解決短網址問題
+            def shortlink_converter(url):
+                resp = requests.get(url)
+                return resp.url
+
+            # 連到fcc事實查核中心
+            partial_url = ''.join(url_link_list_remove_dot)
+            url='https://news.google.com/'+str(partial_url)
+            original_url=shortlink_converter(url)
+            print("original"+original_url)
+            htmlfile=requests.get(original_url,headers=headers,timeout=5)
+            if htmlfile.status_code==requests.codes.ok:
+                print("成功連線到fcc")
+            htmlfile.encoding='utf-8'
+
+            #開始使用bs4 解析
+            objsoup=BeautifulSoup(htmlfile.text,"lxml")
+            title=objsoup.find('h2',{"class":"node-title"})
+            print(title.text)
+            error_str='錯誤'
+            partial_error_str='部份錯誤'
+            real_str='事實釐清'
+            if error_str in title.text:
+                fcc_result="錯誤"
+                #print("錯誤!")
+            elif partial_error_str in title.text:
+                fcc_result="部份錯誤"
+                #print("部分錯誤")
+            elif real_str in title.text:
+                fcc_result="事實釐清"
+                #print("事實釐清")
+        except:
+            fcc_result='目前查無資料'
+        return fcc_result
 
     # 解決短網址問題
     def shortlink_converter(self, url):
@@ -108,7 +174,7 @@ class News:
         # print("generating html results")
         tmp = "<table>"
         # Add table headers
-        tmp += "<tr><th>新聞標題</th><th>新聞標題關鍵字</th><th>新聞內文關鍵字</th><th>情感分析</th></tr>"
+        tmp += "<tr><th>新聞標題</th><th>新聞標題關鍵字</th><th>新聞內文關鍵字</th><th>情感分析</th><th>事實查覈結果</th></tr>"
         for i in range(len(self.news_title)):
             tmp += "<tr>"
             # Add table cells for each news item
@@ -116,6 +182,7 @@ class News:
             tmp += "<td>" + str(self.news_title_kw[i]) + "</td>"
             tmp += "<td>" + str(self.news_content_kw[i]) + "</td>"
             tmp += "<td>" + str(self.sentiment_analysis[i]) + "</td>"
+            tmp += "<td>" + str(self.fcc_results[i]) + "</td>"
             tmp += "</tr>"
         tmp += "</table>"
         return tmp
@@ -130,8 +197,8 @@ class News:
         cursor = db.cursor()
         for i in range(len(self.news_title)):
             # SQL語法      news_title_kw,news_content_kw,
-            sql = "INSERT INTO news_titles_contents(ID,news_title,news_content,news_link,news_title_kw,news_content_kw,sentiment_analysis,createdDate, post_title, post_kw) VALUES ('0','" + str(self.news_title[i]) + "','" + str(
-                    self.news_content[i]) + "','" + str(self.news_link[i]) + "','" + str(self.news_title_kw[i]) + "','" + str(self.news_content_kw[i]) + "','" + str(self.sentiment_analysis[i]) + "','" + str(self.Now) + "', '"+str(self.src_title)+"', '"+str(self.src_keywords)+"')"
+            sql = "INSERT INTO news_titles_contents(ID,news_title,news_content,news_link,news_title_kw,news_content_kw,sentiment_analysis,createdDate, post_title, post_kw, fcc_result) VALUES ('0','" + str(self.news_title[i]) + "','" + str(
+                    self.news_content[i]) + "','" + str(self.news_link[i]) + "','" + str(self.news_title_kw[i]) + "','" + str(self.news_content_kw[i]) + "','" + str(self.sentiment_analysis[i]) + "','" + str(self.Now) + "', '"+str(self.src_title)+"', '"+str(self.src_keywords)+"'"+str(self.fcc_search(self.news_title_kw))+"')"
             # 執行語法
             try:
                 cursor.execute(sql)
@@ -206,6 +273,7 @@ class News:
         self.news_title_kw.append(news_title_kw)
         self.news_content_kw.append(news_content_kw)
         self.sentiment_analysis.append(sentiments_analysis)
+        self.fcc_results.append(self.fcc_search(news_title_kw))
 
     # Main function to fetch the news
     def domain_check(self, domain, news_url):
